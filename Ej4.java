1b447c7a9aa027c0f71f00361ef6800ca2065fa9
pablomartinezalonso <43781943+pablomartinezalonso@users.noreply.github.com>
2018-10-20 12:06:05 +0200

    Ejercicio 9

---
+++ Ej4.java
@@ -0,0 +1,11 @@
+public class Ejercicio6 {
+ 
+    public static void main(String[] args) {
+ 
+        int i = 0x100;
+        System.out.println(i);
+        i >>>= 1;
+        System.out.println(i);
+    }
+ 
+}
